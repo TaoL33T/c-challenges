52f04b99649b0f75527d5d66e2cfe77ed20d51f2
TaoL33T <alucardsama04off@gmail.com>
2023-10-28 18:22:13 +0300

    odd or even

---
+++ 01.Odd or even/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    // Create a function that takes a `number` and returns a `string` that says if the given number is `odd` or `even`
+    
+    int a;
+    printf("Input the number: ");
+    scanf("%d", &a);
+
+    a % 2 != 0 ? printf("odd") : printf("even");
+
+}
