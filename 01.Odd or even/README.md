919ab99b6a0a06e8a490c7d500d7b5c17e3001f7
TaoL33T <alucardsama04off@gmail.com>
2023-10-28 18:24:26 +0300

    Odd or even update

---
+++ 01.Odd or even/README.md
@@ -0,0 +1,1 @@
+Create a function that takes a `number` and returns a `string` that says if the given number is `odd` or `even`
--- 01.Odd or even/main.c
+++ 01.Odd or even/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 void main() {
-    // Create a function that takes a `number` and returns a `string` that says if the given number is `odd` or `even`
+    // Create a function that takes a `number` 
+    // and returns a `string` that says 
+    // if the given number is `odd` or `even`
     
     int a;
     printf("Input the number: ");
